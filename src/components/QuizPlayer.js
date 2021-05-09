bfc7cf8f70c1e733ba2519536bd4085ba6a3a30a
Victor Munduruca <victormunduruca@gmail.com>
2021-05-09 16:02:10 -0300

    added option for escaping activity using esc

--- src/components/QuizPlayer.js
+++ src/components/QuizPlayer.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect }from "react";
 import { useHistory } from "react-router-dom";
 
-import { message, Card, Row, Col, Switch } from 'antd';
+import { message, Card, Row, Col, Switch , Modal} from 'antd';
 import QuizElement from "./QuizElement";
-import { SmileTwoTone , MehTwoTone, CrownTwoTone} from '@ant-design/icons';
+import { SmileTwoTone , MehTwoTone, CrownTwoTone, InfoCircleTwoTone} from '@ant-design/icons';
 
 
 
@@ -13,12 +13,26 @@ function QuizPlayer(props) {
 
     const [currentQuestion, setCurrentQuestion] = useState(props.questions[0]); //sets the first question as current one
     const [isSweepPaused, setIsSweepPaused] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false); //Used in the escape confirmation
     const history = useHistory(); //Used to go back to home after quiz is finished
 
    currentQuestion.stimulusType == "sound" ? startSweepId = 0 : startSweepId = 1;
     
     
     const [focusItem, setfocusItem] = useState(startSweepId);
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleEscape);
+        return () => window.removeEventListener("keydown", handleEscape);
+    }, []);
+    
+
+    function handleEscape(event) {
+        const keyName = event.key;
+        if(keyName == "Escape") {
+            setIsModalVisible(true);
+        }
+    }
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -47,7 +61,7 @@ function QuizPlayer(props) {
                     setCurrentQuestion(props.questions[currentIndex + 1]);
                 } else {
                     message.success({
-                        content:  "Parabéens, você terminou o quiz!",
+                        content:  "Parabéens, você terminou a atividade!",
                         className: "feedback-message",
                         icon: <CrownTwoTone style={{fontSize: "5rem"}} twoToneColor="#fce33f"/>,
                         duration: 2,
@@ -87,6 +101,18 @@ function QuizPlayer(props) {
                     <QuizElement sweepId = {4} answerId = {3} clickable onElementClicked = {onElementClicked} type = {currentQuestion.answerType} value = {currentQuestion.answers[3].value} focusedItem = {focusItem} />
                 </Col>
             </Row>
+            <Modal 
+                title="Aviso"
+                visible={isModalVisible}
+                onOk={() => {
+                    setIsModalVisible(false)
+                    history.push("/");
+                }}
+                onCancel={() => setIsModalVisible(false)}
+                icon={<InfoCircleTwoTone />}
+            >
+                <p>Tem certeza que gostaria de sair da atividade?</p>
+            </Modal>
         </div>
     );
 }
